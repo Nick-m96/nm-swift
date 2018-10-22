8970f7bc27fa3d6c01f384204fb5db680c8acb63
NICO <nmanograsso@gmail.com>
2018-10-22 15:54:26 -0300

    add view to the info of books

--- WBooks/MainTabBarViewController.swift
+++ WBooks/MainTabBarViewController.swift
@@ -21,11 +21,11 @@ class MainTabBarViewController: UITabBarController {
             let searchController = UISearchController(searchResultsController: nil)
             navigationItem.searchController = searchController
         }
-        let backgroundNav = UIImage(named: "Background-navBar")
-        UINavigationBar.appearance().setBackgroundImage(backgroundNav?.resizableImage(withCapInsets: UIEdgeInsets.zero, resizingMode: .stretch), for: .default)
         UINavigationBar.appearance().titleTextAttributes = [NSAttributedStringKey(rawValue: NSAttributedStringKey.foregroundColor.rawValue): UIColor.white]
-        UINavigationBar.appearance().shadowImage = UIImage()
+        UINavigationBar.appearance().setBackgroundImage(backgroundNav?.resizableImage(withCapInsets: UIEdgeInsets.zero, resizingMode: .stretch), for: .default)
+        let backgroundNav = UIImage(named: "Background-navBar")
         UINavigationBar.appearance().barStyle = .blackOpaque
+        UINavigationBar.appearance().shadowImage = UIImage()
     }
     
     func setupTabBar(){
