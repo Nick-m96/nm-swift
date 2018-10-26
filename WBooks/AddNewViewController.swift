8694e18a2d2e57e535db53f94b99dd79b39ed492
NICO <nmanograsso@gmail.com>
2018-10-26 17:38:21 -0300

    creating reactive button to add photo from camera or photo galery in the add new view

--- WBooks/AddNewViewController.swift
+++ WBooks/AddNewViewController.swift
@@ -35,6 +35,15 @@ class AddNewViewController: UIViewController {
     override func loadView() {
         super.loadView()
         let addNewV = AddNewView.loadFromNib()! as AddNewView
+        addNewV.setupText()
         view.addSubview(addNewV)
+        
+        addNewV.translatesAutoresizingMaskIntoConstraints = false
+        let hor = addNewV.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        let ver = addNewV.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        let height = addNewV.heightAnchor.constraint(equalTo: view.heightAnchor)
+        let wid = addNewV.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40)
+        view.addConstraints([hor, ver, height, wid])
+//        addNewV.imgBook.reactive.pressed
     }
 }
