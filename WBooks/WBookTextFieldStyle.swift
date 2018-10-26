5945b28dfe95d4bda7365c02f7a88b66248e6f04
NICO <nmanograsso@gmail.com>
2018-10-26 12:18:23 -0300

    the style of the Input is already

--- WBooks/WBookTextFieldStyle.swift
+++ WBooks/WBookTextFieldStyle.swift
@@ -14,7 +14,12 @@ class WBookTextFieldStyle: UITextField {
         super.layoutSubviews()
         self.placeholder = "Book\'s name"
         self.borderStyle = .none
-        
+        self.layer.backgroundColor = UIColor.white.cgColor
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowRadius = 0.0
     }
 
 }
