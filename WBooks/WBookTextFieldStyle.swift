044e715631f83ae05b527432fa7fe8a4fbeb062f
NICO <nmanograsso@gmail.com>
2018-10-29 15:05:58 -0300

    delete unnecessary code and coments

--- WBooks/WBookTextFieldStyle.swift
+++ WBooks/WBookTextFieldStyle.swift
@@ -13,14 +13,14 @@ class WBookTextFieldStyle: UITextField {
 
     override func layoutSubviews() {
         super.layoutSubviews()
-        self.placeholder = "BOOK_NAME".localized()
-        self.borderStyle = .none
-        self.layer.backgroundColor = UIColor.white.cgColor
-        self.layer.masksToBounds = false
-        self.layer.shadowColor = UIColor.gray.cgColor
-        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
-        self.layer.shadowOpacity = 0.3
-        self.layer.shadowRadius = 0.0
+        placeholder = "BOOK_NAME".localized()
+        borderStyle = .none
+        layer.backgroundColor = UIColor.white.cgColor
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        layer.shadowOpacity = 0.3
+        layer.shadowRadius = 0.0
     }
 
 }
